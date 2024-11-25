bda36da847cef4894c08f38f1cc3572f1245d740
zejie123 <ze-jie-kong-@hotmail.com>
2024-11-25 19:54:12 +0800

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zejie123/Desktop/imu_qt
-# Build directory: /home/zejie123/Desktop/imu_qt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(imu_test "/home/zejie123/Desktop/imu_qt/build/imu_test")
-set_tests_properties(imu_test PROPERTIES  _BACKTRACE_TRIPLES "/home/zejie123/Desktop/imu_qt/CMakeLists.txt;25;add_test;/home/zejie123/Desktop/imu_qt/CMakeLists.txt;0;")
